aa641b3adc6009c870305eef3cb60615e4b0d16e
d4nmc <danielmcgee.work@gmail.com>
2021-07-14 12:48:05 +0100

    trying exercise 1. cant get b to work as intended

--- script.js
+++ script.js
@@ -96,4 +96,107 @@
         //     Check if there is a local variable IF NOT then check if there is a global variable
         // If there is no local or global variable then one is added to the global scope!
 
-        
+//* SCOPE = Visibility of a VARIABLE in a file
+
+const someFunction = () => {
+
+    //? LOCAL SCOPE - Lives within the method
+    let name = `Billy bob joe`;
+
+    console.log(`${name} called from the function`);
+
+}
+
+//! We need the brackets to actually invoke the function!!!
+// someFunction();
+
+//? GLOBAL SCOPE - Visibile to everyone / Use it everywhere / Accessible within the whole file
+
+let hello = `hello world`;
+
+//? created without a let/const - VAR and hoists to the top of the file
+let variableWithoutDeclaration = "hi";
+
+// console.log(variableWithoutDeclaration);
+
+const checkBalance = () => {
+    let balance = 10; 
+}
+
+// checkBalance();
+// console.log(balance); // Error cannot SEE balance!
+
+
+
+let check = false; //? <- GLOBAL
+
+const changeCheck = () => {
+    check = true;
+}
+
+console.log(check); //? <- false
+changeCheck();
+console.log(check); //? <- true
+
+
+//? We look within before going else where!!!!
+
+console.log(`----------------`);
+
+
+let flag; // <- GLOBAL VARIABLE
+
+const test = () => {
+    flag = true; 
+    console.log(flag); // true
+    test1();
+    console.log(flag); // false
+}
+
+const test1 = () => {
+    flag = false;
+}
+
+test();
+
+console.log("-------------------");
+
+let x; //? TRUE GLOBAL 
+
+const anotherOne = () => {
+    x = true; //? <- 
+    console.log(x); //? TRUE
+    theNextMethod();
+    console.log(x); //? Uses Global becuase it can't access the local variable in theNextMethod();
+}
+
+const theNextMethod = () => {
+    let x = false; //? LOCAL VARIABLE
+    return;
+}
+
+anotherOne();
+
+//? True true x2
+
+//? TLDR: Always try to use local scopes where possible. If you don't have a local scope to use, use the global! Methods in a file can't READ/SEE LOCAL variables in other METHODS. "What's mine is mine".
+
+// EXERCISE 1
+
+//     Write the following code and assess the output
+//         Create a function
+//         Declare a variable with a value inside it (i.e. let x = 'foo')
+//         Write an if statement that checks if the variable meets a condition
+//         Inside create a local variable
+//         Try to access both variables and asses your output
+
+
+function aliG(){
+      let a = "fu";
+      if (a = "fu") {
+          let b = "bu";
+      }
+      console.log(a);
+      console.log(b);
+    }
+aliG();
