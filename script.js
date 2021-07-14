380b77bf2a0c0f6df1da45c8eb4f3308fb345293
d4nmc <danielmcgee.work@gmail.com>
2021-07-14 11:12:45 +0100

    Q2 Complete AND further notes added

--- script.js
+++ script.js
@@ -35,27 +35,53 @@
 // ! One line Arrow function
 // const subtraction = (a,b) => console.log(a-b);
 
-// EXERCISE
+// EXERCISE 1
 // The major difference between functions and function expressions are that functions are used globally making it available throughout our code. whereas function expressions are limited to where the function is available keeping the global scope light and maintain clean syntax.
 
 // Create a function that takes in 2 parameters num1 and num2 and subtracts the two numbers.
 
-function subT(num1, num2) {
-    return num1 - num2;
-}
-console.log (subT(50,10));
+// function subT(num1, num2) {
+//     return console.log(num1 - num2);
+// }
 
-// const subtraction = (num1, num2) => console.log(10-300); // nope
+// subtract(50,10);
 
+// const arrowFunctionSubtract = (num1, num2) => {
+
+// return console.log(num1-num2);
+// }
+
+// arrowFunctionSubtract(-60,-110);
+
+
+// EXERCISE 2
 // Create a function expression called welcome that take in name, age,gender as a parameters. The outcome should be like so:
 // e.g. "My name is Felix Cited, i am 20 years old and of gender Male"
 
 
-const welcome = function(name,age,gender){
-    return console.log(`My name is ${name}, I am ${age} years old and I identify as a ${gender}`);
+// const welcome = function(name,age,gender){
+//     return console.log(`My name is ${name}, I am ${age} years old and I identify as a ${gender}`);
     
-    }
+//     }
 
-    welcome("Dwayne Johnson",48,"Rock");
-   
-   
+//     welcome("Dwayne Johnson",48,"Rock");
+//     welcome("Felix Cited",34,"Male");
+
+  
+// EXERCISE 3    
+// Create an arrow function called powerUp that takes in two values n1 and n2. The arrow function will return the power of the two numbers.
+
+//    e.g.
+    
+//         n1=2, n2=3 will return 8 (2 x 2 x 2);
+    
+//         n1=3, n2=3 will return 27 (3 x 3 x 3);
+    
+//         hint use Math.pow()
+
+// Solution from QA
+
+// const powerUp = (n1, n2) => Math.pow (n1, n2); 
+
+//     console.log(powerUp(-3,3));
+
